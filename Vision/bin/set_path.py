c4a4e0b53b520e69e02e9fe7bccb10c8cfb21383
SaurabhsWani <er.saurabhwani1@gmail.com>
2021-05-04 10:29:47 +0530

    V0.0.5-8

--- Vision/bin/set_path.py
+++ Vision/bin/set_path.py
@@ -1,41 +1,51 @@
 import os
+import subprocess
 import sys
 import pyttsx3 as spk
+
 def serach(app):# Function for the seraching app in C drive & adding path in pth file
 	try:
+		spk.speak("Searching for  "+app)
+		print('10%...')
 		pth=os.popen("dir C:\\{ap}.exe /b/s".format(ap=app)).read()
 	except Exception as e:
 		print(e)
 	if pth != "File Not Found":
-		f=open("C:\\pth.txt", 'a')
+		f=open("E:\\pth.txt", 'a')
 		f.write('{pth}'.format(pth=pth))
 		f.close()
-
+		print('100%...')
+		return True
+	print('100%...')
+	return False
 
 def launch(app):#Launching app using path in the pth file
-	f=open("C:\\pth.txt","r")
-	l=f.readlines()
+	try:
+		f=open("E:\\pth.txt","r")
+		l=f.readlines()
+	except:
+		f=open("E:\\pth.txt","w")
+		l=[]
 	pth=None
 	for i in l:
-		if app in i:
+		if app.upper() in i.upper():
 			pth=i[:-1]
-			break
-			f.close()
-	os.popen("start /wait {pth}".format(pth=pth))
-
-app= sys.argv[1] #App to open
-try:# cheking for the environment variable if found launching directly
-	os.popen("start /wait {pth}".format(pth=app))
-	spk.speak(app + " launched successfully....")
-except:#if not found environment variable then
-	try:# launching app by using path in pth file
-		launch(app)
-		spk.speak(app + " launched successfully....")
-	except:# is path not found in the file then serching for app in c drive if path found then adding in the pth file
-		spk.speak("seraching for app...")
-		serach(app)
-		try:# launching app by using path in pth file
-			launch(app)
+			spk.speak("Launching  "+app)
+			p=subprocess.Popen("{pth}".format(pth=pth))
 			spk.speak(app + " launched successfully....")
-		except:# if app not found in c drive 
+			p.communicate()
+			f.close()
+			return True
+	return False
+
+app=sys.argv[1] #App to open
+if launch(app):
+	spk.speak("Closing  "+app)
+else:
+	if serach(app):
+		if launch(app):
+			spk.speak("Closing  "+app)
+		else:
 			spk.speak("App may not be installed yet")
+	else:
+		spk.speak("App may not be installed yet")
